eeaf6f3794f5fe08f9fd7d59f0dda007f24ae6d2
Mosamagdy <mosaelwayly@gmail.com>
2025-07-30 20:33:41 +0300

    Update page.tsx

--- src/app/Post/[id]/page.tsx
+++ src/app/Post/[id]/page.tsx
@@ -1,45 +1,49 @@
-import { CardContent, Container, Grid2 } from '@mui/material'
-import React from 'react'
-import { Margin, Padding } from '@mui/icons-material'
-import axios from 'axios'
-import { cookies } from 'next/headers'
-import { Post } from '@/interfieses/post'
-import CardComponent from '../../_component/CardCombonnt/CartCmponent'
-import { Params } from 'next/dist/server/request/params'
+// src/app/Post/[id]/page.tsx
 
-export default async function Page( {params} : {params:Params} ) {
+import { Container } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import React from 'react';
+import axios from 'axios';
+import { cookies } from 'next/headers';
+import { Post } from '@/interfieses/post';
+import CardComponent from '../../_component/CardCombonnt/CartCmponent';
 
-const { id } = await params; 
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
 
-   const CookesDatat = await cookies()
-  const tokenData  =  CookesDatat.get ('token')
-  
-  async  function getAllpostt(){
-    return await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}posts/${id} `,{
-      headers : {
-        token: tokenData?.value
-      }
-      }).then((res)=>{
-        return res.data.post
-    })
-  }
-  const post : Post = await getAllpostt()
+export default async function Page({ params }: PageProps) {
+  const { id } = params;
+
+  const tokenData = cookies().get('token');
+
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}posts/${id}`, {
+    headers: {
+      token: tokenData?.value || '',
+    },
+  });
+
+  const post: Post = response.data.post;
 
   return (
-    <div style={{ 
-      backgroundColor: 'var(--main-color)',       
-      minHeight: '100vh', 
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}>
+    <div
+      style={{
+        backgroundColor: 'var(--main-color)',
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <Container maxWidth="md">
-        <Grid2 container justifyContent="center" alignItems="center" >
-          <Grid2 >
+        <Grid2 container justifyContent="center" alignItems="center">
+          <Grid2>
             <CardComponent singel={false} postDetails={post} />
           </Grid2>
         </Grid2>
-      </Container>                    
+      </Container>
     </div>
-  )
+  );
 }
