3dea126d6cdb74564d3309b0a94ae72430e86052
Mosamagdy <mosaelwayly@gmail.com>
2025-07-30 20:13:42 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   env: {
     NEXT_PUBLIC_BASE_URL: 'https://linked-posts.routemisr.com/',
   },
